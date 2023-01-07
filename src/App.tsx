3b34d33371256355dc56d9e46e2999c2a77a48cc
Paul DeVito <pdevito3@gmail.com>
2023-01-07 12:49:18 -0500

    update: skeleton of basic auth setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,44 @@
-import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useAuthUser } from "./apis/auth";
+
+const queryClient = new QueryClient();
 
 function App() {
-  const [count, setCount] = useState(0);
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Main />
+    </QueryClientProvider>
+  );
+}
 
-  return <div className="bg-violet-500">hello with tailwindcss</div>;
+function Main() {
+  const { isLoggedIn, username, logoutUrl } = useAuthUser();
+  return (
+    <div className="p-20">
+      {!isLoggedIn ? (
+        <a
+          href="/bff/login?returnUrl=/"
+          className="inline-block px-4 py-2 text-base font-medium text-center text-white bg-blue-500 border border-transparent rounded-md hover:bg-opacity-75"
+        >
+          Login
+        </a>
+      ) : (
+        <div className="flex-shrink-0 block">
+          <div className="flex items-center">
+            <div className="ml-3">
+              <p className="block text-base font-medium text-blue-500 md:text-sm">{`Hi, ${username}!`}</p>
+              <a
+                href={logoutUrl?.value}
+                className="block mt-1 text-sm font-medium text-blue-200 hover:text-blue-500 md:text-xs"
+              >
+                Logout
+              </a>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/apis/auth.tsx
@@ -0,0 +1,52 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const claimsKeys = {
+  claim: ["claims"],
+};
+
+const config = {
+  headers: {
+    "X-CSRF": "1",
+  },
+};
+
+const fetchClaims = async () =>
+  axios.get("/bff/user", config).then((res) => res.data);
+
+function useClaims() {
+  return useQuery(
+    claimsKeys.claim,
+    async () => {
+      return fetchClaims();
+    },
+    {
+      retry: false,
+    }
+  );
+}
+
+function useAuthUser() {
+  const { data: claims, isLoading } = useClaims();
+
+  let logoutUrl = claims?.find((claim: any) => claim.type === "bff:logout_url");
+  let nameDict =
+    claims?.find((claim: any) => claim.type === "name") ||
+    claims?.find((claim: any) => claim.type === "sub");
+  let username = nameDict?.value;
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    setIsLoggedIn(!!username);
+  }, [username]);
+
+  return {
+    username,
+    logoutUrl,
+    isLoading,
+    isLoggedIn,
+  };
+}
+
+export { useAuthUser };
